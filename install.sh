ad4d1c8530ae1d2c4942f592678ae8c5df19b772
Penelope Gwen <penelope@pogmom.me>
2023-12-13 18:46:29 +0000

    1

--- install.sh
+++ install.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 
-lsblk -o NAME,MOUNTPOINT -n -l | awk '$2 == "" {print $1}'
+echo -e "device\ttype\tlabel\tformat\tsize"
+
+print_device_info() {
+    local device=$1
+    local indent=$2
+
+    # Get device info
+    local type=$(lsblk -no TYPE $device)
+    local label=$(lsblk -no LABEL $device)
+    local format=$(lsblk -no FSTYPE $device)
+    local size=$(lsblk -no SIZE $device)
+
+    # Print device info
+    echo -e "$indent$device\t$type\t$label\t$format\t$size"
+
+    # Check if the device has partitions
+    local partitions=($(lsblk -no NAME $device | grep -E "${device}[0-9]+"))
+    for partition in "${partitions[@]}"; do
+        print_device_info "$partition" "$indent    "
+    done
+}
+
+# Get a list of unmounted block devices
+devices=($(lsblk -rno NAME,PATH,MOUNTPOINT | awk '$3 == "" {print $2}'))
+
+# Iterate over unmounted devices
+for device in "${devices[@]}"; do
+    print_device_info "$device" ""
+done
