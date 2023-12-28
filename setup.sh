ed881ce084a48ccaf56a689c3c31c1563b6ccce2
Penelope Gwen <penelope@pogmom.me>
2023-12-27 23:44:38 -0700

    kinda working

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@
 
 #./newuser.sh
 
-#dist=$(cat /root/.config/sapphic-linux/distribution)
-dist=$(cat ./distribution)
+dist=$(cat /root/.config/sapphic-linux/distribution)
+#dist=$(cat ./distribution)
 
 ./dists/${dist}.sh
 
-#./hostname.sh
+./hostname.sh
 
 #./done.sh
